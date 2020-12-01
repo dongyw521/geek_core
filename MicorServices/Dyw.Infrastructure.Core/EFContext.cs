9e16dc88e94bcec78c26ac2a3850a08418c7d51f
yanwei.dong <yanwei.dong@wetax.com.cn>
2020-12-01 18:07:36 +0800

    1201

--- MicorServices/Dyw.Infrastructure.Core/EFContext.cs
+++ MicorServices/Dyw.Infrastructure.Core/EFContext.cs
@@ -2,14 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 
 namespace Dyw.Infrastructure.Core
 {
-    public class EFContext : DbContext
+    public class EFContext : DbContext, IUnitOfWork
     {
-        public EFContext(DbContextOptions options) : base(options)
-        {
+        protected IMediator mediator;
 
+        public EFContext(DbContextOptions options,IMediator _mediator) : base(options)
+        {
+            mediator = _mediator;
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await base.SaveChangesAsync(cancellationToken);
+            throw new NotImplementedException();
         }
 
 
---
+++ MicorServices/Dyw.Infrastructure.Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dyw.Infrastructure.Core
+{
+    /// <summary>
+    /// 放在Repository中，实现统一的保存，减少数据库连接次数
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+    }
+}
