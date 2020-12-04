e9b55405861735f519511da7a4a67db3af19c5df
yanwei.dong <yanwei.dong@wetax.com.cn>
2020-12-04 18:03:31 +0800

    1204

--- MicorServices/Dyw.Infrastructure.Core/EFContext.cs
+++ MicorServices/Dyw.Infrastructure.Core/EFContext.cs
@@ -5,23 +5,101 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Dyw.Domain.Abstractions;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Dyw.Infrastructure.Core
 {
-    public class EFContext : DbContext, IUnitOfWork
+    public class EFContext : DbContext, IUnitOfWork, ITransaction
     {
         protected IMediator mediator;
 
-        public EFContext(DbContextOptions options,IMediator _mediator) : base(options)
+        public EFContext(DbContextOptions options, IMediator _mediator) : base(options)
         {
             mediator = _mediator;
         }
 
+        #region 事务
+
+        private IDbContextTransaction _currentTransaction;
+
+        public bool HasActiveTransaction => _currentTransaction != null;
+
+        public Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return null;
+            _currentTransaction = Database.BeginTransaction();
+            return Task.FromResult(_currentTransaction);
+        }
+
+        public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+        {
+            if (transaction == null) throw new ArgumentNullException(nameof(transaction));
+            if (transaction != _currentTransaction) throw new InvalidOperationException($"The transaction {transaction.TransactionId} is not the current!");
+            try
+            {
+                await SaveChangesAsync();
+                transaction.Commit();
+            }
+            catch
+            {
+                RollBackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public IDbContextTransaction GetCurrentTransaction()
+        {
+            return _currentTransaction;
+        }
+
+        public void RollBackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        #endregion
+
+        #region IUnitOfWork
+        /// <summary>
+        /// 一次性持久化到库，并触发领域事件
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
         {
             await base.SaveChangesAsync(cancellationToken);
-            throw new NotImplementedException();
+            var domainEntities = this.ChangeTracker.Entries<Entity>().Where(e => e.Entity.DomainEvents != null && e.Entity.DomainEvents.Any());
+            var domainEvents = domainEntities.SelectMany(e => e.Entity.DomainEvents).ToList();
+            domainEntities.ToList().ForEach(e => e.Entity.ClearDomainEvent());
+            foreach (var _event in domainEvents)
+            {
+                await mediator.Publish(_event);//触发当前被操作实体上绑定的所有领域事件
+            }
+            return true;
         }
+
+        #endregion
 
 
     }
---
+++ MicorServices/Dyw.Infrastructure.Core/ITransaction.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyw.Infrastructure.Core
+{
+    public interface ITransaction
+    {
+        bool HasActiveTransaction { get; }
+
+        IDbContextTransaction GetCurrentTransaction();
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+
+        Task CommitTransactionAsync(IDbContextTransaction transaction);
+
+        void RollBackTransaction();
+    }
+}
